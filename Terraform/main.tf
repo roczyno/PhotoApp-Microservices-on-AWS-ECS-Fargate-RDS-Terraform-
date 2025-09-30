e31a60343c03964f20ef6fa6f6050ddfbb6a09da
roczyno <adiabaroczyno9@gmail.com>
2025-09-30 14:19:34 +0000

    add(ecr):create the ecr repositories

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -107,5 +107,43 @@ resource "aws_route_table_association" "private" {
 
 
 
+resource "aws_ecr_repository" "microservices" {
+  for_each             = var.microservices
+  name                 = "${var.cluster_name}-${each.key}"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name        = "${var.cluster_name}-${each.key}"
+    Service     = each.key
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "microservices" {
+  for_each   = var.microservices
+  repository = aws_ecr_repository.microservices[each.key].name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 10 images"
+        selection = {
+          tagStatus     = "tagged"
+          tagPrefixList = ["v"]
+          countType     = "imageCountMoreThan"
+          countNumber   = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
 
 
